e9ef401b5f8a4d23b875c1d04e5aded7a66f10b5
IvambergSilva <ivambergisilva@gmail.com>
2023-10-02 23:16:11 -0300

    :sparkles: feat: Configurando e conectando a Api

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,13 @@
+import { api } from "@/services/apiClient";
+import Router from "next/router";
+import { destroyCookie, setCookie } from "nookies";
 import { createContext, ReactNode, useState } from "react";
 
 interface AuthContextData {
     user: IUserProps;
     isAuthenticated: boolean;
     signIn: (credentials: ISignInProps) => Promise<void>;
+    signOut: () => void;
 }
 
 interface IUserProps {
@@ -28,11 +32,41 @@ export function AuthProvider({ children }: IAuthProviderProps) {
 
     const isAuthenticated = !!user
 
-    async function signIn({ email, password }: ISignInProps) { }
+    async function signIn({ email, password }: ISignInProps) {
+        try {
+            const response = await api.post('/session', {
+                email, password
+            })
+
+            const { id, name, token } = response.data;
+
+            setCookie(undefined, '@bfood.token', token, {
+                maxAge: 60 * 60 * 24 * 30,
+                path: '/'
+            })
+
+            setUser({ id, name, email });
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+            Router.push('/dashboard');
+        } catch (error) {
+            console.log('Error ao acessar: ', error);
+        }
+    }
 
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
     )
+}
+
+export function signOut() {
+    try {
+        destroyCookie(undefined, '@bfood.token')
+        Router.push('/')
+    } catch {
+        console.log('Erro ao deslogar');
+    }
 }
